9fa2849d0607045a63675fc898220d5da117bb36
Justin Hold <36420615+jhold1982@users.noreply.github.com>
2025-04-09 16:04:03 -0500

    Word Dictionary added

--- Scramblet/GameDictionary.swift
+++ Scramblet/GameDictionary.swift
@@ -6,3 +6,53 @@
 //
 
 import Foundation
+
+struct GameDictionary {
+	
+	// Declares a private Set collection variable named 'words' that stores unique String values
+	private var words = Set<String>()
+	
+	
+	init() {
+		
+		guard let url = Bundle.main.url(forResource: "dictionary", withExtension: "txt") else {
+			fatalError("Failed to locate dictionary.txt")
+		}
+		
+		guard let string = try? String(contentsOf: url, encoding: .utf8) else {
+			fatalError("Failed to load dictionary.txt")
+		}
+		
+		let allWords = string.components(separatedBy: "\n")
+		
+		words = Set(allWords.filter { $0.count <= 6 })
+	}
+	
+	func canForm(_ source: String, from target: String) -> Bool {
+		
+		var target = target
+		
+		for letter in source {
+			if let pos = target.firstIndex(of: letter) {
+				target.remove(at: pos)
+			} else {
+				return false
+			}
+		}
+		
+		return true
+	}
+	
+	func spellableWords(from target: String) -> [String] {
+		
+		var result = [String]()
+		
+		for word in words {
+			if canForm(word, from: target) {
+				result.append(word)
+			}
+		}
+		
+		return result
+	}
+}
