e0c719a8e88e3ab9105f1ce30e3347e608519c6f
Justin Hold <36420615+jhold1982@users.noreply.github.com>
2025-04-09 12:46:08 -0500

    Initial Commit

---
+++ Scramblet/ScrambletApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrambletApp.swift
+//  Scramblet
+//
+//  Created by Justin Hold on 4/9/25.
+//
+
+import SwiftUI
+
+@main
+struct ScrambletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
