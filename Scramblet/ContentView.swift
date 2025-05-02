85431bcbfc0b26720fb33d686849811d67bc3cb4
Justin Hold <36420615+jhold1982@users.noreply.github.com>
2025-05-02 14:17:46 -0500

    Game Complete

--- Scramblet/ContentView.swift
+++ Scramblet/ContentView.swift
@@ -18,6 +18,9 @@ struct ContentView: View {
 	@State private var letters = [Letter]()
 	@State private var currentWord = [Letter]()
 	
+	@State private var currentProgress = 0.0
+	@State private var totalProgress = 1.0
+	
 	let columns = Array(
 		repeating: GridItem(
 			.flexible(
@@ -33,6 +36,10 @@ struct ContentView: View {
     var body: some View {
 		
 		VStack(spacing: 20) {
+			
+			ProgressView("Level Progress", value: currentProgress, total: totalProgress)
+				.tint(currentProgress / totalProgress > 0.8 ? .green : nil)
+			
 			LazyVGrid(columns: columns) {
 				ForEach(spellableWords, id: \.self) { word in
 					Text(
@@ -44,6 +51,7 @@ struct ContentView: View {
 					.frame(maxWidth: .infinity, alignment: .center)
 				}
 			}
+			.padding()
 			
 			HStack {
 				ForEach(currentWord) { letter in
@@ -79,8 +87,18 @@ struct ContentView: View {
 				}
 			}
 			
-			Button("Submit", action: submit)
-				.disabled(currentWord.count < 3)
+			Divider()
+			
+			HStack(spacing: 50) {
+				
+				Button("Shuffle", action: shuffle)
+				
+				Button("Submit", action: submit)
+					.disabled(currentWord.count < 3)
+			}
+			
+			Button("Next Level", action: load)
+				.disabled(currentProgress / totalProgress < 0.8)
         }
 		.padding()
 		.onAppear(perform: load)
@@ -107,6 +125,11 @@ struct ContentView: View {
 		letters = targetWord.shuffled().map {
 			Letter(text: String($0))
 		}
+		
+		totalProgress = Double(spellableWords.map(\.count).reduce(0, +))
+		
+		currentProgress = 0
+		foundWords.removeAll()
 	}
 	
 	func use(_ letter: Letter) {
@@ -132,6 +155,8 @@ struct ContentView: View {
 		
 		if spellableWords.contains(spelled) {
 			foundWords.insert(spelled)
+			
+			currentProgress += Double(spelled.count)
 		}
 		
 		currentWord.removeAll()
@@ -152,6 +177,12 @@ struct ContentView: View {
 			
 		}
 		return result
+	}
+	
+	func shuffle() {
+		withAnimation {
+			letters.shuffle()
+		}
 	}
 	
 }
